acc6e7ca303181bc62b673e8ea403057adfbcebb
João Cerri <joaocerri06@gmail.com>
2025-09-05 16:52:55 -0300

    initial commit SIMPLE PROJECT

---
+++ Projeto-Simples/Projeto-Simples/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Projeto_Simples
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double CostPrice { get; set; }
+        public double SellPrice { get; set; }
+        public string Supplier { get; set; }
+
+        public void ClientsProduct() { }
+    }
+
+}
